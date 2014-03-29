d885415fbd724f2edbd1f88a53a80a42270f5da2
G00fY2 <thomas-w-@web.de>
2014-03-30 00:48:42 +0100

    test edit

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
 	
 	public static void main(String[] args) throws Exception {
 		
-		String apiFile = "F:\\apikey.txt";
+		String apiFile = "F:\\workspace\\apikey.txt";
 		String apiKey = null;
 		
 
